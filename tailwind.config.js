2e8cb1a4513dd716ad52c68362ce7599b3279462
Thexasan <129182612+Thexasan@users.noreply.github.com>
2023-04-08 17:05:42 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1002px'},
+      // => @media (max-width: 1002px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
+  }
+}
